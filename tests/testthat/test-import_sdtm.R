4c870c6507afb732cbe0c6d4feecb9346735c974
Bill Denney <wdenney@humanpredictions.com>
2025-02-16 12:09:02 -0500

    Test import errors

--- tests/testthat/test-import_sdtm.R
+++ tests/testthat/test-import_sdtm.R
@@ -16,6 +16,17 @@ test_that("import_sdtm", {
   )
 })
 
+test_that("import_sdtm expected errors", {
+  expect_error(
+    import_sdtm(path = "foo"),
+    regexp = 'The following were not found as directories or files: "foo"'
+  )
+  expect_error(
+    suppressMessages(import_sdtm(path = test_path("example-sdtm/suppdm.xpt"), auto_supp = TRUE)),
+    regexp = "Domain DM was not found when trying to auto-combine --SUPP data with SUPPDM"
+  )
+})
+
 test_that("import_sdtm auto_supp automatically merges --SUPP domains", {
   expect_message(
     ld_sdtm <- import_sdtm(path = test_path("example-sdtm")),
